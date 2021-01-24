e3fc39cfea2dd29ee6268d6b373c4526f3ad0fd9
Holger Lamm <holger@e-gitt.net>
2021-01-24 21:47:47 +0100

    Lese DC power vom Wechselrichter

--- src/modules/wr_tripower/__init__.py
+++ src/modules/wr_tripower/__init__.py
@@ -7,18 +7,17 @@ class TRIPOWER(PVModul):
    """SMA Tripower"""
 
    def setup(self, config):
+      super().setup(config)
       host = config[self.configprefix + '_ip']
       assert host is not None, "Host für %s notwenig! (Setting %s_ip)" % (self.configprefix, self.configprefix)
       if config.get(self.configprefix + '_type') == 'modbus':
          self.instance = ModbusWR(host)
       else:  # dashboard
          self.instance = SMADASH(host)
-      super().setup(config)
 
    def loop(self):
       try:
-         power, generation = self.instance.read()
-         self.send({'pvwatt': power, 'pvkwh': generation})
+         self.send(self.instance.read())
       except ConnectionError:
          self.send({})
 
--- src/modules/wr_tripower/modbuswr.py
+++ src/modules/wr_tripower/modbuswr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import struct
+from typing import List
 from pymodbus.client.sync import ModbusTcpClient
 
 class ModbusWR:
@@ -10,25 +11,41 @@ class ModbusWR:
     """
     def __init__(self, ip):
         self.host = ip
+        self.client = ModbusTcpClient(self.host, port=502, timeout=10)
+
+
+    @staticmethod
+    def decode_s32(value: List[int]) -> int:
+       # To enforce signed decoding, there seems to be no better way.
+       all = bytes.fromhex(format(value[0], '04x') + format(value[1], '04x'))
+       return struct.unpack('>i', all)[0]
+
+    @staticmethod
+    def decode_u32(value: List[int]) -> int:
+       all = format(resp.registers[0], '04x') + format(resp.registers[1], '04x')
+       return int(all, 16)
 
     def read(self):
         try:
-            client = ModbusTcpClient(self.host, port=502, timeout=10)
-
             #pv watt
-            resp = client.read_holding_registers(30775, 2, unit=3)  # Wirkleistung alle Außenleiter, S32 W
-            # To enforce signed decoding, there seems to be no better way.
-            all = bytes.fromhex(format(resp.registers[0], '04x') + format(resp.registers[1], '04x'))
-            power = struct.unpack('>i', all)[0]
+            resp = self.client.read_holding_registers(30775, 2, unit=3)  # Wirkleistung alle Außenleiter, S32 W
+            power = self.decode_s32(resp.registers)
             if power < 0:
                 power = 0
             power = -power
 
             #pv Wh
-            resp = client.read_holding_registers(30529, 2, unit=3)  # Total AC power, U32 Wh
-            all = format(resp.registers[0], '04x') + format(resp.registers[1], '04x')
-            generation = int(all, 16)/1000
-            return power, generation
+            resp = self.client.read_holding_registers(30529, 6, unit=3)  # Total AC power, U32 [Wh]
+            generation = self.decode_u32(resp.registers) / 1000
+
+            # DCA, S32[mA] / DCU, S32[0.01V] / DCW, S32[W]
+            resp = self.client.read_holding_registers(30769, 6, unit=3)
+            dca = self.decode_s32(resp.registers[0:1]) / 1000
+            dcu = self.decode_s32(resp.registers[2:3]) / 100
+            dcp = self.decode_s32(resp.registers[4:5])
+            self.logging.info(f"DCA: {dca} DCU: {dcu} DCP: {dcp}")
+
+            return {'pvwatt': power, 'pvkwh': generation}
         except:
             raise ConnectionError
 
