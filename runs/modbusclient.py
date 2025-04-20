17de890ce69cfc8312c17deb9a6d2177e91d8bc7
Holger Lamm <theHolgi@posteo.de>
2025-04-20 22:49:04 +0200

    Modbus client

--- runs/modbusclient.py
+++ runs/modbusclient.py
@@ -3,13 +3,26 @@
 from pymodbus.client import ModbusTcpClient
 import struct
 import argparse
+import time
+
 
 def readregister(client, reg: int, unit: int, length: int) -> int:
-      resp = client.read_input_registers(reg, count=length, slave=unit)
-      print(f"Read: {resp}")
+   client.connect()
+   # time.sleep(10)
+   if 10000 <= reg < 20000:
+      method = client.read_coils
+   elif 30000 <= reg <= 40000:
+      method = client.read_input_registers
+   elif 40000 <= reg <= 50000:
+      method = client.read_holding_registers
+   else:
+      raise ValueError("Invalid register " + str(reg))
+   method = client.read_holding_registers
+   resp = method(reg, count=length, slave=unit)
+   print(f"Read: {resp}")
 
-      all = bytes.fromhex(format(resp.registers[0], '04x') + format(resp.registers[1], '04x'))
-      return struct.unpack('>i', all)[0]
+   all = bytes.fromhex(format(resp.registers[0], '04x') + format(resp.registers[1], '04x'))
+   return struct.unpack('>i', all)[0]
 
 parser = argparse.ArgumentParser()
 parser.add_argument('host', type=str)
